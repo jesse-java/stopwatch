45600a11ea62a5cf61da13d5a05ecc16f9d61a10
Jesse Naldo <naldojesse@jnaldoweb.com>
2018-02-17 17:45:54 -0800

    started work on model, controller and index, Timers controller will receive and print out query parameters from index

--- src/com/naldojesse/com/models/Timer.java
+++ src/com/naldojesse/com/models/Timer.java
@@ -1,4 +1,19 @@
 package com.naldojesse.com.models;
 
-public class Timer {
+
+public class Timer implements java.io.Serializable{
+    private Timer start;
+    private Timer stop;
+
+    public Timer() {
+        start = null;
+        stop = null;
+    }
+
+    public Timer(Timer start) {
+        this.start = start;
+        stop = null;
+    }
+
+
 }
